c5c953c278e306e0fd1978c313eba43a2e644aee
Muhammad <kaizokuny666@gmail.com>
2024-11-08 17:06:45 +0300

    update

--- src/game.js
+++ src/game.js
@@ -1,11 +1,12 @@
 import Main from "./mainScene.js";
 
-eruda.init()
+
+
 
 let config = {
     type: Phaser.AUTO,
-    width: 1280, 
-    height: 720,
+    width: 1920, 
+    height: 1080,
     backgroundColor: 0x000000,
     scene: [Main],
     pixelArt: true,
@@ -20,8 +21,9 @@ let config = {
     
     scale: {
         mode: Phaser.Scale.FIT,
-    }
-  
+    },
 };
 
 let game = new Phaser.Game(config);
+
+console.log(Phaser.Scenes)
